2df3e6cee72438fe8c43925b580d19cefeb27231
imemilyhuang <emilyhuang405@gmail.com>
2023-01-11 00:25:11 -0600

    update

--- src/routes/Acknowledgments.js
+++ src/routes/Acknowledgments.js
@@ -72,13 +72,13 @@ const Acknowledgments = () => {
           <p className="p" style={{marginBottom: "3rem", textAlign: "center", maxWidth: "76rem"}}>
             This project was made possible by these amazing open-source projects and platforms!
           </p>
-          <a target="_blank" rel="noopener noreferrer" href="https://create-react-app.dev/" className="p" style={styles.linkStyle}>Create React App</a>
-          <a target="_blank" rel="noopener noreferrer" href="https://reactjs.org/" className="p" style={styles.linkStyle}>React</a>
-          <a target="_blank" rel="noopener noreferrer" href="https://reactrouter.com/en/main" className="p" style={styles.linkStyle}>React Router</a>
+          <a target="_blank" rel="noopener noreferrer" href="https://github.com/facebook/create-react-app/blob/main/LICENSE" className="p" style={styles.linkStyle}>Create React App</a>
+          <a target="_blank" rel="noopener noreferrer" href="https://github.com/facebook/react/blob/main/LICENSE" className="p" style={styles.linkStyle}>React</a>
+          <a target="_blank" rel="noopener noreferrer" href="https://github.com/remix-run/react-router/blob/main/LICENSE.md" className="p" style={styles.linkStyle}>React Router</a>
+          <a target="_blank" rel="noopener noreferrer" href="https://github.com/willmcpo/body-scroll-lock/blob/master/LICENSE" className="p" style={{...styles.linkStyle, marginBottom: "3rem"}}>Body Scroll Lock</a>
+          <a target="_blank" rel="noopener noreferrer" href="https://github.com/firebase/firebase-tools/blob/master/LICENSE" className="p" style={styles.linkStyle}>Firebase CLI</a>
           <a target="_blank" rel="noopener noreferrer" href="https://firebase.google.com/" className="p" style={styles.linkStyle}>Firebase</a>
-          <a target="_blank" rel="noopener noreferrer" href="https://www.npmjs.com/package/firebase-tools" className="p" style={styles.linkStyle}>Firebase CLI</a>
           <a target="_blank" rel="noopener noreferrer" href="https://vercel.com/" className="p" style={styles.linkStyle}>Vercel</a>
-          <a target="_blank" rel="noopener noreferrer" href="https://github.com/willmcpo/body-scroll-lock" className="p" style={{...styles.linkStyle, marginBottom: "3rem"}}>Body Scroll Lock</a>
         </div>
       </div>
 
